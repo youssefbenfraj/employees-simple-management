c96bc1f6d3d1ebf512d08500789cfcb611731cb7
Youssef Ben Fraj <93080017+youssefbenfraj@users.noreply.github.com>
2023-06-14 15:18:36 +0100

    Update CorsConfig.java

--- backend/src/main/java/com/selimhorri/pack/configs/cors/CorsConfig.java
+++ backend/src/main/java/com/selimhorri/pack/configs/cors/CorsConfig.java
@@ -1,45 +1,32 @@
-package com.selimhorri.pack.configs.cors;
-
-import java.util.Arrays;
-
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-
 @Configuration
 public class CorsConfig {
-	
-	// public static final String allowedOrigin = "http://localhost:4200";
-        	
-	
-	@Bean
-	public CorsFilter corsFilter() {
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://192.168.146.133:4200", "http://192.168.146.133:4200/"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-															"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-															"Access-Control-Request-Method", "Access-Control-Request-Headers"));
-		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-															"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		
-		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter(urlBasedCorsConfigurationSource);
-	}
-	
-	
-	
+
+    @Bean
+    public CorsFilter corsFilter() throws UnknownHostException {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+
+        // Retrieve the server IP address or hostname dynamically
+        String serverAddress = InetAddress.getLocalHost().getHostAddress();
+
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://" + serverAddress + ":4200", "http://" + serverAddress + ":4200/"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
 }
-
-
-
-
-
-
-
-
