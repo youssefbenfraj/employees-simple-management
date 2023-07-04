2387258e36f3c05adc6fab9319543044bc2db500
Youssef Ben Fraj <93080017+youssefbenfraj@users.noreply.github.com>
2023-07-04 08:46:01 +0100

    Update CorsConfig.java

--- backend/src/main/java/com/selimhorri/pack/configs/cors/CorsConfig.java
+++ backend/src/main/java/com/selimhorri/pack/configs/cors/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://104.41.149.95:4200", "http://104.41.149.95:4200/"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://4.156.178.253:4200", "http://4.156.178.253:4200/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 															"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 															"Access-Control-Request-Method", "Access-Control-Request-Headers"));
